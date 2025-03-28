0a267d953b2c2a3b8f4bbbbc729c90a9fd3c31d5
sxtxri <136388089+sxtxri@users.noreply.github.com>
2025-03-28 11:22:05 +0500

    Update main.py

--- 3Task/main.py
+++ 3Task/main.py
@@ -1,1 +1,23 @@
+s = input()
 
+stack = []
+flag = True
+bracket = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+}
+
+for x in s:
+    if x in bracket.keys():
+        stack.append(x)
+    elif len(stack) > 0 and bracket[stack[-1]] == x:
+        stack.pop()
+    else:
+        flag = False
+        break
+
+if len(stack) > 0 or flag == False:
+    print(False)
+else:
+    print(True)
